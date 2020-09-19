0e1de08a3bb35ce173c9dc974bb9d8615fed6ce8
RishonAnthony <70851332+Rishonanthony22@users.noreply.github.com>
2020-09-19 09:19:20 +0530

    Add files via upload

---
+++ crumpled paper 1/Dustbin.js
@@ -0,0 +1,6 @@
+class Dustbin {
+    constructor(x,y,width,height) {
+      super(x,y,width,height);
+      this.image = loadImage("images/dustbin.png");
+    }
+  };
---
+++ crumpled paper 1/Paper.js
@@ -0,0 +1,6 @@
+class Paper extends BaseClass {
+    constructor(x,y) {
+      super(x,y,width,height);
+      this.image = loadImage("images/crumpledpaper.png");
+    }
+  };
